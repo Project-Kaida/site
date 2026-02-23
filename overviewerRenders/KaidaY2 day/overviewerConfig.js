419f3f87e109d1ece1f6b1d5b17c4513a49cedf1
DariuszKrych <darek.krych@gmail.com>
2026-02-23 14:03:22 +0100

    Mid Updating for Year2

---
+++ overviewerRenders/KaidaY2 day/overviewerConfig.js
@@ -0,0 +1,73 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "ProjectKaida_Y2"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1771782322",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        },
+        "attribution": null
+    },
+    "tilesets": [
+        {
+            "name": "Year 2 Map (day)",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "survivalday",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "ProjectKaida_Y2",
+            "last_rendertime": 1771684944,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                16,
+                81,
+                80
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                16,
+                81,
+                80
+            ],
+            "north_direction": 0
+        }
+    ]
+};
